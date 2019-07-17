9032d8dec2f053171dddaf6f106013bc9e25b05c
shalam15 <salamajayi@yahoo.com>
2019-07-16 23:01:05 -0400

    added flex box, to see how it will look hint: terrible

--- GSU-Music/src/app/app.module.ts
+++ GSU-Music/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MusicsComponent } from './components/musics/musics.component';
+import { MusicCardComponent } from './components/music-card/music-card.component';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MusicsComponent,
+    MusicCardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ GSU-Music/src/app/components/musics/musics.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { MusicService } from '../../services/music.service';
+
+
+import { Music } from '../../models/Music';
+
+@Component({
+  selector: 'app-musics',
+  templateUrl: './musics.component.html',
+  styleUrls: ['./musics.component.css']
+})
+export class MusicsComponent implements OnInit {
+  musics:Music[];
+
+  constructor(private musicService:MusicService) { }
+
+  ngOnInit() {
+    this.musics = this.musicService.getMusics();
+  }
+
+}
